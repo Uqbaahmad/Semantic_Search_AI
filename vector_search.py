8c477fb5c2fe0395abcbddb9a8982f27590750bc
Uqba Ahmad <104266403+Uqbaahmad@users.noreply.github.com>
2023-05-18 11:04:06 +0530

    Initial commit

---
+++ vector_search.py
@@ -0,0 +1,21 @@
+import pinecone
+from sentence_transformers import SentenceTransformer,util
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+pinecone.init(api_key="a88f41cf-3264-48d8-8a04-953f06214d55", environment = "us-central1-gcp")
+index = pinecone.Index("semantic-search-ai")
+
+def addData(corpusData,url):
+    id = id = index.describe_index_stats()['total_vector_count']
+    for i in range(len(corpusData)):
+        chunk=corpusData[i]
+        chunkInfo=(str(id+i),
+                model.encode(chunk).tolist(),
+                {'title': url,'context': chunk})
+        index.upsert(vectors=[chunkInfo])
+
+def find_match(query,k):
+    query_em = model.encode(query).tolist()
+    result = index.query(query_em, top_k=k, includeMetadata=True)
+    
+    return [result['matches'][i]['metadata']['title'] for i in range(k)],[result['matches'][i]['metadata']['context'] for i in range(k)]
